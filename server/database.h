bd12189af546c17a78b734c5d1a80ce4c5d52211
OrlovRuslan <107631072+Rainkord@users.noreply.github.com>
2026-04-10 21:10:35 +0300

    Add emailExists and updatePasswordByEmail declarations

--- server/database.h
+++ server/database.h
@@ -10,18 +10,18 @@ public:
     static Database &instance();
 
     bool connect(const QString &dbName);
-    bool addUser(const QString &name, const QString &passwordHash, const QString &email);
-    bool checkUser(const QString &name, const QString &passwordHash);
-    bool userExists(const QString &name);
+
+    bool    addUser(const QString &name, const QString &passwordHash, const QString &email);
+    bool    checkUser(const QString &name, const QString &passwordHash);
+    bool    userExists(const QString &name);
+    bool    emailExists(const QString &email);                                  // NEW
     QString getUserEmail(const QString &name);
+    bool    updatePasswordByEmail(const QString &email, const QString &newHash); // NEW
 
 private:
     Database() = default;
-    Database(const Database &) = delete;
-    Database &operator=(const Database &) = delete;
-
-    QSqlDatabase m_db;
     bool createTable();
+    QSqlDatabase m_db;
 };
 
 #endif // DATABASE_H
