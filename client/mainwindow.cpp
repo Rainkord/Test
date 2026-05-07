bbc6cf136d015619da994b59217075259f17f448
OrlovRuslan <107631072+Rainkord@users.noreply.github.com>
2026-05-07 17:24:29 +0300

    fix: mainwindow.cpp correct signals matching actual .h files

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,66 +1,27 @@
 #include "mainwindow.h"
-#include "authwidget.h"
-#include "regwidget.h"
-#include "verifywidget.h"
-#include "graphwidget.h"
 #include "clientsingleton.h"
+#include "taskdialog.h"
+#include "schemadialog.h"
 
-#include <QStackedWidget>
-#include <QVBoxLayout>
 #include <QApplication>
 #include <QScreen>
-#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 
-static const int IDX_AUTH   = 0;
-static const int IDX_REG    = 1;
-static const int IDX_VERIFY = 2;
-static const int IDX_GRAPH  = 3;
+static const char* GH_BG     = "#0d1117";
+static const char* GH_PANEL  = "#161b22";
+static const char* GH_BORDER = "#30363d";
+static const char* GH_TEXT   = "#e6edf3";
+static const char* GH_ACCENT = "#388bfd";
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle(QString::fromUtf8("\xd0\x93\xd1\x80\xd0\xb0\xd1\x84\xd0\xb8\xd0\xba \xd1\x84\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd0\xb8"));
-
-    stackedWidget = new QStackedWidget(this);
-    setCentralWidget(stackedWidget);
-
-    authWidget   = new AuthWidget(this);
-    regWidget    = new RegWidget(this);
-    verifyWidget = new VerifyWidget(this);
-    graphWidget  = new GraphWidget(this);
-
-    stackedWidget->addWidget(authWidget);    // 0
-    stackedWidget->addWidget(regWidget);     // 1
-    stackedWidget->addWidget(verifyWidget);  // 2
-    stackedWidget->addWidget(graphWidget);   // 3
-
-    stackedWidget->setCurrentIndex(IDX_AUTH);
-
-    // Auth
-    connect(authWidget, &AuthWidget::loginSuccess, this, &MainWindow::onLoginSuccess);
-    connect(authWidget, &AuthWidget::showRegister, this, [this]() {
-        regWidget->clearFields();
-        stackedWidget->setCurrentIndex(IDX_REG);
-    });
-
-    // Reg
-    connect(regWidget, &RegWidget::registrationSuccess, this, &MainWindow::onRegistrationSuccess);
-    connect(regWidget, &RegWidget::showLogin, this, [this]() {
-        authWidget->clearFields();
-        stackedWidget->setCurrentIndex(IDX_AUTH);
-    });
-    connect(regWidget, &RegWidget::verificationRequired,
-            this, &MainWindow::onVerificationRequired);
-
-    // Verify
-    connect(verifyWidget, &VerifyWidget::verificationSuccess,
-            this, &MainWindow::onVerificationSuccess);
-    connect(verifyWidget, &VerifyWidget::backToRegister, this, [this]() {
-        stackedWidget->setCurrentIndex(IDX_REG);
-    });
-
-    // Graph
-    connect(graphWidget, &GraphWidget::logout, this, &MainWindow::onLogout);
+    setupUI();
+    connectSignals();
 
     QScreen *screen = QApplication::primaryScreen();
     if (screen) {
@@ -72,20 +33,127 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow() {}
 
-void MainWindow::onLoginSuccess(const QString &login)
+void MainWindow::setupUI()
 {
-    graphWidget->setUserLogin(login);
-    graphWidget->updateGraph();
-    stackedWidget->setCurrentIndex(IDX_GRAPH);
+    centralWidget = new QWidget(this);
+    centralWidget->setStyleSheet(QString("background-color: %1;").arg(GH_BG));
+    setCentralWidget(centralWidget);
+
+    mainVLayout = new QVBoxLayout(centralWidget);
+    mainVLayout->setContentsMargins(0, 0, 0, 0);
+    mainVLayout->setSpacing(0);
+
+    // Top bar
+    QWidget *topBar = new QWidget(centralWidget);
+    topBar->setFixedHeight(48);
+    topBar->setStyleSheet(QString(
+        "QWidget { background-color: %1; border-bottom: 1px solid %2; }"
+    ).arg(GH_PANEL).arg(GH_BORDER));
+
+    topBarLayout = new QHBoxLayout(topBar);
+    topBarLayout->setContentsMargins(16, 0, 16, 0);
+    topBarLayout->setSpacing(8);
+
+    appTitleLabel = new QLabel(QString::fromUtf8("\xd0\x93\xd1\x80\xd0\xb0\xd1\x84\xd0\xb8\xd0\xba \xd1\x84\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd0\xb8"), topBar);
+    appTitleLabel->setStyleSheet(QString(
+        "QLabel { color: %1; font-size: 14px; font-weight: bold; background: transparent; border: none; }"
+    ).arg(GH_TEXT));
+
+    QString btnStyle = QString(
+        "QPushButton { background-color: %1; color: %2; border: 1px solid %3;"
+        " border-radius: 6px; padding: 4px 12px; font-size: 12px; }"
+        "QPushButton:hover { background-color: #21262d; }"
+    ).arg(GH_PANEL).arg(GH_TEXT).arg(GH_BORDER);
+
+    taskBtn = new QPushButton(QString::fromUtf8("\xd0\x97\xd0\xb0\xd0\xb4\xd0\xb0\xd0\xbd\xd0\xb8\xd0\xb5"), topBar);
+    taskBtn->setStyleSheet(btnStyle);
+    taskBtn->hide();
+
+    schemaBtn = new QPushButton(QString::fromUtf8("\xd0\xa1\xd1\x85\xd0\xb5\xd0\xbc\xd0\xb0"), topBar);
+    schemaBtn->setStyleSheet(btnStyle);
+    schemaBtn->hide();
+
+    topBarLayout->addWidget(appTitleLabel);
+    topBarLayout->addStretch();
+    topBarLayout->addWidget(taskBtn);
+    topBarLayout->addWidget(schemaBtn);
+
+    mainVLayout->addWidget(topBar);
+
+    stackedWidget = new QStackedWidget(centralWidget);
+    stackedWidget->setStyleSheet("background: transparent;");
+    mainVLayout->addWidget(stackedWidget, 1);
+
+    authWidget   = new AuthWidget(centralWidget);
+    regWidget    = new RegWidget(centralWidget);
+    verifyWidget = new VerifyWidget(centralWidget);
+    graphWidget  = new GraphWidget(centralWidget);
+    resetWidget  = new ResetWidget(centralWidget);
+
+    stackedWidget->addWidget(authWidget);    // 0 IDX_AUTH
+    stackedWidget->addWidget(regWidget);     // 1 IDX_REG
+    stackedWidget->addWidget(verifyWidget);  // 2 IDX_VERIFY
+    stackedWidget->addWidget(graphWidget);   // 3 IDX_GRAPH
+    stackedWidget->addWidget(resetWidget);   // 4 IDX_RESET
+
+    stackedWidget->setCurrentIndex(IDX_AUTH);
 }
 
-void MainWindow::onLogout()
+void MainWindow::connectSignals()
+{
+    // AuthWidget signals: loginSuccess(login), showRegister()
+    connect(authWidget, &AuthWidget::loginSuccess, this, [this](const QString &login) {
+        graphWidget->setUserLogin(login);
+        graphWidget->updateGraph();
+        taskBtn->show(); schemaBtn->show();
+        stackedWidget->setCurrentIndex(IDX_GRAPH);
+    });
+    connect(authWidget, &AuthWidget::showRegister, this, &MainWindow::onShowRegister);
+
+    // RegWidget signals: registrationSuccess(login), showAuth()
+    connect(regWidget, &RegWidget::registrationSuccess, this, &MainWindow::onRegistrationSuccess);
+    connect(regWidget, &RegWidget::showAuth, this, &MainWindow::onShowAuth);
+
+    // VerifyWidget signals: verificationSuccess(login), backToAuth()
+    connect(verifyWidget, &VerifyWidget::verificationSuccess, this, &MainWindow::onVerificationSuccess);
+    connect(verifyWidget, &VerifyWidget::backToAuth, this, &MainWindow::onBackToAuth);
+
+    // GraphWidget signals: logout()
+    connect(graphWidget, &GraphWidget::logout, this, &MainWindow::onLogout);
+
+    // Top bar buttons
+    connect(taskBtn,   &QPushButton::clicked, this, &MainWindow::onTaskBtnClicked);
+    connect(schemaBtn, &QPushButton::clicked, this, &MainWindow::onSchemaBtnClicked);
+}
+
+// --- Slots ---
+
+void MainWindow::onShowRegister()
+{
+    regWidget->clearFields();
+    stackedWidget->setCurrentIndex(IDX_REG);
+}
+
+void MainWindow::onShowAuth()
 {
     authWidget->clearFields();
     stackedWidget->setCurrentIndex(IDX_AUTH);
 }
 
-void MainWindow::onVerificationRequired(const QString &login)
+void MainWindow::onBackToAuth()
+{
+    authWidget->clearFields();
+    stackedWidget->setCurrentIndex(IDX_AUTH);
+}
+
+void MainWindow::onLogout()
+{
+    authWidget->clearFields();
+    taskBtn->hide(); schemaBtn->hide();
+    stackedWidget->setCurrentIndex(IDX_AUTH);
+}
+
+void MainWindow::onShowVerifyAuth(const QString &login)
 {
     verifyWidget->setLogin(login);
     stackedWidget->setCurrentIndex(IDX_VERIFY);
@@ -95,6 +163,7 @@ void MainWindow::onVerificationSuccess(const QString &login)
 {
     graphWidget->setUserLogin(login);
     graphWidget->updateGraph();
+    taskBtn->show(); schemaBtn->show();
     stackedWidget->setCurrentIndex(IDX_GRAPH);
 }
 
@@ -102,5 +171,29 @@ void MainWindow::onRegistrationSuccess(const QString &login)
 {
     graphWidget->setUserLogin(login);
     graphWidget->updateGraph();
+    taskBtn->show(); schemaBtn->show();
     stackedWidget->setCurrentIndex(IDX_GRAPH);
+}
+
+void MainWindow::onShowReset()
+{
+    stackedWidget->setCurrentIndex(IDX_RESET);
+}
+
+void MainWindow::onResetSuccess()
+{
+    authWidget->clearFields();
+    stackedWidget->setCurrentIndex(IDX_AUTH);
+}
+
+void MainWindow::onTaskBtnClicked()
+{
+    TaskDialog dlg(this);
+    dlg.exec();
+}
+
+void MainWindow::onSchemaBtnClicked()
+{
+    SchemaDialog dlg(this);
+    dlg.exec();
 }
--- client/taskdialog.cpp
+++ client/taskdialog.cpp
@@ -1,8 +1,8 @@
 #include "taskdialog.h"
 #include <QApplication>
 #include <QScreen>
-#include <QPixmap>
 #include <QScrollArea>
+#include <QPixmap>
 
 static const char* GH_BG      = "#0d1117";
 static const char* GH_SURFACE = "#161b22";
@@ -53,64 +53,77 @@ void TaskDialog::setupUI()
         return card;
     };
 
-    // title
-    titleLabel = new QLabel(QString::fromUtf8("\xe2\x84\xb9\xef\xb8\x8f  \xd0\x98\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd0\xb8\xd1\x8f \xd0\xbe \xd0\xb7\xd0\xb0\xd0\xb4\xd0\xb0\xd0\xbd\xd0\xb8\xd0\xb8"));
+    // ---- Title
+    titleLabel = new QLabel();
+    titleLabel->setText(QString::fromUtf8("\xe2\x84\xb9\xef\xb8\x8f  ") +
+                        QString::fromUtf8("\xd0\x98\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd0\xb8\xd1\x8f \xd0\xbe \xd0\xb7\xd0\xb0\xd0\xb4\xd0\xb0\xd0\xbd\xd0\xb8\xd0\xb8"));
     titleLabel->setStyleSheet(QString(
         "QLabel { color: %1; font-size: 18px; font-weight: bold; background: transparent; border: none; }"
     ).arg(GH_TEXT));
     mainLayout->addWidget(titleLabel);
 
-    // work title card
+    // ---- Work title card
     workTitleLabel = new QLabel();
     workTitleLabel->setTextFormat(Qt::RichText);
     workTitleLabel->setWordWrap(true);
     workTitleLabel->setStyleSheet("background: transparent; border: none;");
-    workTitleLabel->setText(
-        QString("<span style='color:%1; font-size:11pt; font-weight:bold;'>")
-            .arg(GH_ACCENT)
-        + QString::fromUtf8("\xd0\xa0\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xb0")
-        + ":</span><br>"
-        + QString("<span style='color:%1; font-size:10pt;'>").arg(GH_TEXT)
-        + QString::fromUtf8("\xd0\x98\xd1\x81\xd1\x81\xd0\xbb\xd0\xb5\xd0\xb4\xd0\xbe\xd0\xb2\xd0\xb0\xd0\xbd\xd0\xb8\xd0\xb5 \xd0\xb8 \xd0\xbf\xd0\xbe\xd1\x81\xd1\x82\xd1\x80\xd0\xbe\xd0\xb5\xd0\xbd\xd0\xb8\xd0\xb5 \xd0\xb3\xd1\x80\xd0\xb0\xd1\x84\xd0\xb8\xd0\xba\xd0\xb0 \xd1\x84\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd0\xb8")
-        + "</span>"
-    );
+    {
+        QString acc = QString(GH_ACCENT);
+        QString txt = QString(GH_TEXT);
+        workTitleLabel->setText(
+            QString("<span style='color:%1; font-size:11pt; font-weight:bold;'>").arg(acc) +
+            QString::fromUtf8("\xd0\xa0\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xb0") +
+            QString(":</span><br>") +
+            QString("<span style='color:%1; font-size:10pt;'>").arg(txt) +
+            QString::fromUtf8("\xd0\x98\xd1\x81\xd1\x81\xd0\xbb\xd0\xb5\xd0\xb4\xd0\xbe\xd0\xb2\xd0\xb0\xd0\xbd\xd0\xb8\xd0\xb5 \xd0\xb8 \xd0\xbf\xd0\xbe\xd1\x81\xd1\x82\xd1\x80\xd0\xbe\xd0\xb5\xd0\xbd\xd0\xb8\xd0\xb5 \xd0\xb3\xd1\x80\xd0\xb0\xd1\x84\xd0\xb8\xd0\xba\xd0\xb0 \xd1\x84\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd0\xb8") +
+            QString("</span>")
+        );
+    }
     mainLayout->addWidget(makeCard(workTitleLabel));
 
-    // group card
+    // ---- Group card
     groupLabel = new QLabel();
     groupLabel->setTextFormat(Qt::RichText);
     groupLabel->setWordWrap(true);
     groupLabel->setStyleSheet("background: transparent; border: none;");
-    groupLabel->setText(
-        QString("<span style='color:%1; font-size:11pt; font-weight:bold;'>").arg(GH_ACCENT)
-        + QString::fromUtf8("\xd0\x93\xd1\x80\xd1\x83\xd0\xbf\xd0\xbf\xd0\xb0")
-        + ":</span><br>"
-        + QString("<span style='color:%1; font-size:10pt;'>").arg(GH_TEXT)
-        + "251-372, "
-        + QString::fromUtf8("\xd0\xb2\xd0\xb0\xd1\x80\xd0\xb8\xd0\xb0\xd0\xbd\xd1\x82")
-        + " 9</span>"
-    );
+    {
+        QString acc = QString(GH_ACCENT);
+        QString txt = QString(GH_TEXT);
+        groupLabel->setText(
+            QString("<span style='color:%1; font-size:11pt; font-weight:bold;'>").arg(acc) +
+            QString::fromUtf8("\xd0\x93\xd1\x80\xd1\x83\xd0\xbf\xd0\xbf\xd0\xb0") +
+            QString(":</span><br>") +
+            QString("<span style='color:%1; font-size:10pt;'>").arg(txt) +
+            QString("251-372, ") +
+            QString::fromUtf8("\xd0\xb2\xd0\xb0\xd1\x80\xd0\xb8\xd0\xb0\xd0\xbd\xd1\x82") +
+            QString(" 9</span>")
+        );
+    }
     mainLayout->addWidget(makeCard(groupLabel));
 
-    // members card
+    // ---- Members card
     membersLabel = new QLabel();
     membersLabel->setTextFormat(Qt::RichText);
     membersLabel->setWordWrap(true);
     membersLabel->setStyleSheet("background: transparent; border: none;");
-    membersLabel->setText(
-        QString("<span style='color:%1; font-size:11pt; font-weight:bold;'>").arg(GH_ACCENT)
-        + QString::fromUtf8("\xd0\xa7\xd0\xbb\xd0\xb5\xd0\xbd\xd1\x8b \xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xbf\xd1\x8b")
-        + ":</span><br>"
-        + QString("<span style='color:%1; font-size:10pt;'>").arg(GH_TEXT)
-        + "&#8226; "
-        + QString::fromUtf8("\xd0\x9e\xd1\x80\xd0\xbb\xd0\xbe\xd0\xb2 \xd0\xa0\xd1\x83\xd1\x81\xd0\xbb\xd0\xb0\xd0\xbd")
-        + " &#8226; "
-        + QString::fromUtf8("\xd0\xa2\xd0\xb8\xd0\xbc\xd0\xbe\xd1\x84\xd0\xb5\xd0\xb5\xd0\xb2 \xd0\x90\xd0\xbd\xd1\x82\xd0\xbe\xd0\xbd")
-        + "</span>"
-    );
+    {
+        QString acc = QString(GH_ACCENT);
+        QString txt = QString(GH_TEXT);
+        membersLabel->setText(
+            QString("<span style='color:%1; font-size:11pt; font-weight:bold;'>").arg(acc) +
+            QString::fromUtf8("\xd0\xa7\xd0\xbb\xd0\xb5\xd0\xbd\xd1\x8b \xd0\xb3\xd1\x80\xd1\x83\xd0\xbf\xd0\xbf\xd1\x8b") +
+            QString(":</span><br>") +
+            QString("<span style='color:%1; font-size:10pt;'>").arg(txt) +
+            QString("&#8226; ") +
+            QString::fromUtf8("\xd0\x9e\xd1\x80\xd0\xbb\xd0\xbe\xd0\xb2 \xd0\xa0\xd1\x83\xd1\x81\xd0\xbb\xd0\xb0\xd0\xbd") +
+            QString(" &#8226; ") +
+            QString::fromUtf8("\xd0\xa2\xd0\xb8\xd0\xbc\xd0\xbe\xd1\x84\xd0\xb5\xd0\xb5\xd0\xb2 \xd0\x90\xd0\xbd\xd1\x82\xd0\xbe\xd0\xbd") +
+            QString("</span>")
+        );
+    }
     mainLayout->addWidget(makeCard(membersLabel));
 
-    // formula image card
+    // ---- Formula card
     {
         auto *card = new QWidget();
         card->setStyleSheet(QString(
@@ -124,9 +137,10 @@ void TaskDialog::setupUI()
         hdr->setTextFormat(Qt::RichText);
         hdr->setStyleSheet("background: transparent; border: none;");
         hdr->setText(
-            QString("<b style='color:%1; font-size:11pt;'>").arg(GH_ACCENT)
-            + QString::fromUtf8("\xd0\xa4\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd1\x8f \xe2\x84\x969:")
-            + "</b>"
+            QString("<b style='color:%1; font-size:11pt;'>").arg(QString(GH_ACCENT)) +
+            QString::fromUtf8("\xd0\xa4\xd1\x83\xd0\xbd\xd0\xba\xd1\x86\xd0\xb8\xd1\x8f ") +
+            QString("&#8470;9:") +
+            QString("</b>")
         );
         cl->addWidget(hdr);
 
@@ -136,16 +150,27 @@ void TaskDialog::setupUI()
         QPixmap pm(":/formula_task.png");
         if (!pm.isNull())
             formulaLabel->setPixmap(pm.scaledToWidth(460, Qt::SmoothTransformation));
-        else
-            formulaLabel->setText("<span style='color:#ff6b6b;'>Image not found</span>");
+        else {
+            formulaLabel->setTextFormat(Qt::RichText);
+            formulaLabel->setText(
+                "<p style='color:#e6edf3; font-size:10pt;'>"
+                "<b>f(x) =</b><br>"
+                "<span style='color:#f85149;'>|x| &minus; 2, x &lt; &minus;2</span><br>"
+                "<span style='color:#3fb950;'>x&sup2; + x + 1, &minus;2 &le; x &lt; 2</span><br>"
+                "<span style='color:#58a6ff;'>|x &minus; 2| + 1, x &ge; 2</span>"
+                "</p>"
+            );
+        }
         cl->addWidget(formulaLabel);
         mainLayout->addWidget(card);
     }
 
     mainLayout->addStretch();
 
-    closeBtn = new QPushButton();
-    closeBtn->setText(QString::fromUtf8("\xe2\x9c\x95  \xd0\x97\xd0\xb0\xd0\xba\xd1\x80\xd1\x8b\xd1\x82\xd1\x8c"));
+    closeBtn = new QPushButton(
+        QString::fromUtf8("\xe2\x9c\x95  ") +
+        QString::fromUtf8("\xd0\x97\xd0\xb0\xd0\xba\xd1\x80\xd1\x8b\xd1\x82\xd1\x8c")
+    );
     closeBtn->setStyleSheet(QString(
         "QPushButton { background-color: %1; color: %2; border: 1px solid %3;"
         " border-radius: 6px; padding: 8px 20px; font-size: 13px; }"
