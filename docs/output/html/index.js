6bac301b56357c1522bc342dabf18d1177990405
Orlov Ruslan <orlovruslan2016@gmail.com>
2026-05-29 00:39:27 +0300

    fix(docs): исправить формулы, Qt версию, ФИО участников, PlantUML, test_cases
    - Формулы в README, wiki, mainpage.dox: x²+1/3sin(x)/e^0.5x → |x·a|-2/bx²+x+1/|x-2|+c
    - Qt 6 → Qt 5 в README.md (соответствует коду и Dockerfile)
    - ТМП → ТИМП в mainpage.dox
    - Участник 2/3 → реальные ФИО (Карелин, Серёгина, Воробьёва, Сарафанов)
    - usecase_diagram.puml: \n → <br> в лейбле <<extend>>
    - test_cases.md: заголовки категорий вынесены из таблицы
    - Перегенерирована Doxygen документация

---
+++ docs/output/html/index.js
@@ -0,0 +1,9 @@
+var index =
+[
+    [ "Кусочная функция (вариант 9)", "index.html#func", null ],
+    [ "Архитектура", "index.html#arch", null ],
+    [ "Компоненты сервера", "index.html#server", null ],
+    [ "Компоненты клиента", "index.html#client", null ],
+    [ "Протокол обмена", "index.html#protocol", null ],
+    [ "Технологии", "index.html#tech", null ]
+];
